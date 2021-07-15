746145b902a4f462197dbf403aac9c1aefa9932d
Haim Gilboa <Haim.Gilboa@amdocs.com>
2021-07-15 22:58:56 +0300

    Initial commit - generate spring-boot project

---
+++ src/main/java/com/maba/osads/OSAdsApplication.java
@@ -0,0 +1,13 @@
+package com.maba.osads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OSAdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OSAdsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maba/osads/OSAdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maba.osads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OSAdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
